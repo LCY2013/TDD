0f0395ce4f8302a65cd90d02ea4a5cf63de4ff6e
fufeng <15281205719@163.com>
2023-05-21 15:57:02 +0800

    doc: README.md

--- args/src/test/java/org/fufeng/tdd/ArgsTest.java
+++ args/src/test/java/org/fufeng/tdd/ArgsTest.java
@@ -13,7 +13,7 @@ public class ArgsTest {
     // Single Option:
 
     // -Bool -l
-    /*@Test
+    @Test
     public void should_set_boolean_option_to_true_if_flag_present() {
         BooleanOption option = Args.parse(BooleanOption.class, "-l");
 
@@ -27,10 +27,10 @@ public class ArgsTest {
         assertFalse(option.logging());
     }
 
-    static record BooleanOption(@Option("l") boolean logging) {}*/
+    static record BooleanOption(@Option("l") boolean logging) {}
 
     // -Integer -p 8080
-    /*@Test
+    @Test
     public void should_set_integer_option_to_true_if_flag_present() {
         IntegerOption option = Args.parse(IntegerOption.class, "-p", "8080");
 
@@ -62,7 +62,7 @@ public class ArgsTest {
         assertEquals("", option.directory());
     }
 
-    static record StringOption(@Option("d") String directory) {}*/
+    static record StringOption(@Option("d") String directory) {}
 
     // Multiple Option:
     // -l -p 8080 -d /usr/logs
@@ -70,19 +70,31 @@ public class ArgsTest {
 
     @Test
     public void should_set_mutiple_option_to_true_if_flag_present() {
+        // SUT Args.parse
+
+        // exercise
         MultiOptions options = Args.parse(MultiOptions.class, "-l", "-p", "8080", "-d", "/usr/logs");
+
+        // verify
         assertTrue(options.logging());
         assertEquals(8080, options.port());
         assertEquals("/usr/logs", options.directory());
+
+        // teardown
     }
 
+    // setup
     static record MultiOptions(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {}
 
     @Test
     public void should_throw_illegal_option_exception_if_annotation_not_present() {
-        IllegalOptionException e = assertThrows(IllegalOptionException.class, () -> Args.parse(OptionsWithoutAnnotation.class));
+        // before
+        IllegalOptionException e = /*verify*/assertThrows(IllegalOptionException.class, () -> /* execrise */Args.parse(OptionsWithoutAnnotation.class));
 
+        // verify
         assertEquals("port", e.getOption());
+
+        // teardown
     }
 
     static record OptionsWithoutAnnotation(@Option("l") boolean logging, int port, @Option("d") String directory) {}
